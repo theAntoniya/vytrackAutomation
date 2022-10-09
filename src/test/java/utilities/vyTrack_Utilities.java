631dbb1301e2c85bd6b73f11a53c56a3dd0f5032
Umut <umut19950123@gmail.com>
2022-10-09 01:50:07 -0700

    vyTrack log in utility created

--- src/test/java/utilities/vyTrack_Utilities.java
+++ src/test/java/utilities/vyTrack_Utilities.java
@@ -1,4 +1,18 @@
 package utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class vyTrack_Utilities {
+
+
+    public static void logInVyTrack(WebDriver driver,String userName,String password){
+        WebElement userNameBox = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+        userNameBox.sendKeys(userName);
+        WebElement passwordBox = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
+        passwordBox.sendKeys(password);
+        WebElement logInBtn = driver.findElement(By.xpath("//button[@id='_submit']"));
+        logInBtn.click();
+    }
 }
