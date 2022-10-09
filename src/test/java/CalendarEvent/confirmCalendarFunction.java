0fa210f826a8dfa506e302950e12515dea616c59
Umut <umut19950123@gmail.com>
2022-10-09 01:28:38 -0700

    Before method created

--- src/test/java/CalendarEvent/confirmCalendarFunction.java
+++ src/test/java/CalendarEvent/confirmCalendarFunction.java
@@ -1,5 +1,23 @@
 package CalendarEvent;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import utilities.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
 public class confirmCalendarFunction {
+    WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+        //1. Open Chrome browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to https://qa1.vytrack.com/user/login
+        driver.get("https://qa1.vytrack.com/user/login");
+    }
 
 }
