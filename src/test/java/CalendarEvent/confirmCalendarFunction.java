8f61ab4dec01bea5362b46d2d6d5a17784805223
Umut <umut19950123@gmail.com>
2022-10-09 19:50:50 -0700

    test2 verify error massage completed

--- src/test/java/CalendarEvent/confirmCalendarFunction.java
+++ src/test/java/CalendarEvent/confirmCalendarFunction.java
@@ -23,6 +23,9 @@ public class confirmCalendarFunction {
     public static String password="UserUser123";
 
    public static String expectedDefaultValue = "1";
+   public static String expectedErrorMassage ="This value should not be blank.";
+
+
     @BeforeMethod
     public void setupMethod(){
         //1. Open Chrome browser
@@ -47,7 +50,16 @@ public class confirmCalendarFunction {
     }
 
     @Test
-    public void testErrorMassage_AC2(){
+    public void testErrorMassage_AC2() throws InterruptedException {
+
+        for (String userName : userNames) {
+            VerifyErrorMassage(driver,userName,expectedErrorMassage);
+            logOutVyTrack(driver);
+            Thread.sleep(1000);
+
+        }
+
+
 
     }
 
@@ -88,6 +100,43 @@ public class confirmCalendarFunction {
 
     }
 
+    public static void VerifyErrorMassage(WebDriver driver,String userName,String expectedErrorMassage) throws InterruptedException {
+        // log in with utility
+        VyTrack_Utilities.logInVyTrack(driver,userName,password);
+
+        // locate activities dropdown
+        WebElement activitiesElement = driver.findElement(By.linkText("Activities"));
+
+        //use action function to mouse move to activities dropdown
+        Actions action = new Actions(driver);
+        action.moveToElement(activitiesElement).perform();
+        // locate the calendar event
+        WebElement calendarEvent = driver.findElement(By.linkText("Calendar Events"));
+
+        calendarEvent.click(); // click it to go create go calendar event page
+
+        //locating create calendar event button
+        WebElement createCalenderEventBtn = driver.findElement(By.xpath("//a[@title='Create Calendar event']"));
+
+        Thread.sleep(3000);// need time to run to page
+        createCalenderEventBtn.click();// clicking it to go create calendar event page
+
+        //locate repeat selection
+        Thread.sleep(2000);
+        WebElement repeatSelection  = driver.findElement(By.xpath("//input[@data-name='recurrence-repeat']"));
+        repeatSelection.click(); // select it
+        //locate repeat every day(s) box
+        WebElement repeatEveryDays= driver.findElement(By.xpath("(//label/input[@class='recurrence-subview-control__number'])[1]"));
+        repeatEveryDays.clear(); // clear the box
+        // locate error massage
+        WebElement errorMassage = driver.findElement(By.xpath("//span//span//span[.='This value should not be blank.']"));
+        String actualErrorMassageText =errorMassage.getText();//get massage text
+        // verify actual massage
+        Assert.assertEquals(actualErrorMassageText,expectedErrorMassage);
+
+
+    }
+
     public static void logOutVyTrack(WebDriver driver){
         WebElement userDropdown = driver.findElement(By.xpath("//i[@class='fa-caret-down']"));
         userDropdown.click();
@@ -100,7 +149,33 @@ public class confirmCalendarFunction {
 
     @AfterMethod
     public void tearDownMethod(){
-       driver.quit();
+      driver.quit();
     }
 
 }
+
+
+/*
+Test cases #1
+Description:  users see the default repeat day as 1
+Environment: https://qa1.vytrack.com/user/login
+Steps:
+Users are on the homepage
+Click the “Calendar Events” under the Activities
+Click the “Create Calendar Event” button
+Check the Repeat checkbox
+Verify the repeat number is 1
+
+Test cases #2
+Description:  users clear the repat day and see an error message
+Environment: https://qa1.vytrack.com/user/login
+Steps:
+Users are on the homepage
+Click the “Calendar Events” under the Activities
+Click the “Create Calendar Event” button
+Check the Repeat checkbox
+Clear(delete) the number 1
+Verify the app displays “This value should not be blank.”
+
+
+ */
