6164be760460132af16aaf77613753fb6173a6ee
Zorigt <robertganbold@gmail.com>
2022-10-13 16:35:43 -0500

    Updated

--- src/test/java/com/vytrack/tests/TS26_74.java
+++ src/test/java/com/vytrack/tests/TS26_74.java
@@ -15,48 +15,40 @@ public class TS26_74 {
     WebDriver driver;
 
     @BeforeMethod
+    public void setUp() {
 
-    public void setUp () {
 
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
     }
-        @Test
+    @Test
 
-        public void loginWeb(){
+    public void InfoPageValidation () {
 
         driver.get("https://qa1.vytrack.com/user/login");
 
-            WebElement userName = driver.findElement(By.xpath("//input[@id='prependedInput']"));
-            userName.sendKeys("salesmanager263");
-            WebElement passWord = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
 
-            passWord.sendKeys("UserUser123");
+        WebElement userName = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+        userName.sendKeys("salesmanager263");
+        WebElement passWord = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
+        passWord.sendKeys("UserUser123");
 
-            WebElement loginButton = driver.findElement(By.xpath("//*[@class='fa-question-circle']"));
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"_submit\"]"));
+        loginButton.click();
 
-            loginButton.click();
-
-        }
-
-    public void ValidInfoIcon(){
 
         driver.get("https://qa1.vytrack.com/");
+
+
         WebElement clickButton = driver.findElement(By.xpath("//*[@class='fa-question-circle']"));
         clickButton.click();
 
-
     }
-
-        @AfterMethod
-
     public void tearDown (){
 
         driver.quit();
-        }
-
     }
-
+}
 
